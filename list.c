709e994505cc1da7687aad53c15dac8941e4bf4c
Your Name <you@example.com>
2022-03-20 06:39:56 +0000

    new try

--- list.c
+++ list.c
@@ -112,9 +112,9 @@ void * popBack(List * list) {
 void * popCurrent(List * list) {
   if(list->current != NULL){
     if(list->current->next != NULL){
-      list->current->prev->next =list->current->next;
        if(list->current->prev != NULL){
         list->current->next->prev = list->current->prev;
+        list->current->prev->next =list->current->next;
         }else{
         list->head=list->current->next;
         }
